3bd18c33a189e03a9cc8b19078941533a5dd014b
aberm <abermendel@gmail.com>
2019-06-19 16:52:00 -0400

    work with tom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,73 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(POKEMON); //YOUR CODE HERE
+
+  const container = document.getElementById("pokemon-container");
+  const boxText = document.getElementById("pokemon-search-input");
+
+  for (let i = 0; i < POKEMON.length; i++) {
+    const currPoke = POKEMON[i];
+    const card = `<div class="pokemon-card">
+      <div class="pokemon-frame">
+        <h1 class="center-text">${currPoke.name}</h1>
+        <div class="pokemon-image">
+          <img data-id="${
+            currPoke.id
+          }" data-action="flip" class="toggle-sprite" src="${
+      currPoke.sprites.front
+    }">
+        </div>
+      </div>
+    </div>`;
+
+    container.innerHTML += card;
+  }
+
+  boxText.addEventListener("input", event => {
+    console.log(event.target.value);
+
+    container.innerHTML = "";
+
+    const newPokemon = POKEMON.filter(pokemon =>
+      pokemon.name.includes(event.target.value)
+    );
+    newPokemon.forEach(pokemon => {
+      const card = `<div class="pokemon-card">
+        <div class="pokemon-frame">
+          <h1 class="center-text">${pokemon.name}</h1>
+          <div class="pokemon-image">
+            <img data-id="${
+              pokemon.id
+            }" data-action="flip" class="toggle-sprite" src="${
+        pokemon.sprites.front
+      }">
+          </div>
+        </div>
+      </div>`;
+
+      container.innerHTML += card;
+    });
+
+    // for (let i = 0; i < newPokemon.length; i++) {
+    //   const currPoke = newPokemon[i];
+    //   const card = `<div class="pokemon-card">
+    //     <div class="pokemon-frame">
+    //       <h1 class="center-text">${currPoke.name}</h1>
+    //       <div class="pokemon-image">
+    //         <img data-id="${
+    //           currPoke.id
+    //         }" data-action="flip" class="toggle-sprite" src="${
+    //     currPoke.sprites.front
+    //   }">
+    //       </div>
+    //     </div>
+    //   </div>`;
+    //
+    //   container.innerHTML += card;
+    // }
+
+    //console.log(event.target.value);
+  });
+  // grab text inside of it
+  // for each pokemon being displayed
+  // check if pokmon name includes string
+});
